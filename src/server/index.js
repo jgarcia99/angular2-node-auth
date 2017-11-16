ca7a5c7b49297d4992c3d2a1854805ae213ba6e2
Josuel C Garcia <garciajosuel@yahoo.com>
2017-11-16 18:47:54 -0500

    build failing not compiling css still...

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ let database;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/profiles', express.static(path.join(__dirname, 'public')));
+app.use('/profiles', express.static(path.join(__dirname, 'profiles')));
 
 
 app.get('/contacts', (req, res) => {
@@ -38,8 +38,8 @@ app.post('/api/contacts', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    return res.sendfile(path.join(__dirname, 'public/index.html'));
-})
+    return res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 MongoClient.connect(process.env.DB_CONN, (err, db) => {
     console.log('connected to mongodb...');
