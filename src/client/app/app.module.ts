99bbc1d193c94cf955414a661d15b689e65a728c
Josuel C Garcia <garciajosuel@yahoo.com>
2017-11-16 18:09:31 -0500

    shoot me now switched from css to scss so now it's not compiling the sass

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ContactListComponent
+    ContactListComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
